7c88e03aeda28ad4323e7312c2f0555e1e9fbbce
Wanderson-Xavier <wanderson.123sad@gmail.com>
2023-11-28 00:05:26 -0300

    Adicionado IA

--- Assets/Scripts/movimento.cs
+++ Assets/Scripts/movimento.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class movimento : MonoBehaviour
 {
+
+    public float distanciaMin = 1;
+    public Transform alvo;
+
+    private NavMeshAgent navMeshAgent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(transform.position, alvo.position) <= distanciaMin)
+        return;
+
+        navMeshAgent.SetDestination(alvo.position);
     }
 }
